463d614478fc4d9725f41da4b285351015e0cea6
Mikhail Yutman <myutman@gmail.com>
2020-02-23 13:40:41 +0300

    back to visualisation

--- camtrack/_corners.py
+++ camtrack/_corners.py
@@ -231,25 +231,25 @@ def create_cli(build):
         if file_to_dump is not None:
             dump(corner_storage, file_to_dump)
         if show:
-            #click.echo(
-            #    "Press 'q' to stop, 'd' to go forward, 'a' to go backward, 'r' to restart the video"
-            #)
-            #frame = 0
-            #while True:
-            #    grayscale = sequence[frame]
-            #    bgr = draw(grayscale, corner_storage[frame])
-            #    cv2.imshow('Frame', bgr)
-            #    key = chr(cv2.waitKey(20) & 0xFF)
-            #    if key == 'a' and frame > 0:
-            #        frame -= 1
-            #    if key == 'd' and frame + 1 < len(corner_storage):
-            #        frame += 1
-            #    if key == 'r':
-            #        frame = 0
-            #    if key == 'q':
-            #        break
-            for i, image in enumerate(sequence):
-                bgr = draw(image, corner_storage[i])
-                cv2.imwrite('{}.png'.format(i), np.array(bgr * 255, dtype=np.uint8))
+            click.echo(
+                "Press 'q' to stop, 'd' to go forward, 'a' to go backward, 'r' to restart the video"
+            )
+            frame = 0
+            while True:
+                grayscale = sequence[frame]
+                bgr = draw(grayscale, corner_storage[frame])
+                cv2.imshow('Frame', bgr)
+                key = chr(cv2.waitKey(20) & 0xFF)
+                if key == 'a' and frame > 0:
+                    frame -= 1
+                if key == 'd' and frame + 1 < len(corner_storage):
+                    frame += 1
+                if key == 'r':
+                    frame = 0
+                if key == 'q':
+                    break
+            #for i, image in enumerate(sequence):
+            #    bgr = draw(image, corner_storage[i])
+            #    cv2.imwrite('{}.png'.format(i), np.array(bgr * 255, dtype=np.uint8))
 
     return cli
